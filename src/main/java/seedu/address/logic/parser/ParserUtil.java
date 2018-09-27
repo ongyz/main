a1986c0020659e7dc4250b61b22c0eda5734e8ca
SusterDrifter <39788874+SusterDrifter@users.noreply.github.com>
2018-09-27 12:27:30 +0800

    Change logic/parser/ParserUtil.java: omit javadoc

--- src/main/java/seedu/address/logic/parser/ParserUtil.java
+++ src/main/java/seedu/address/logic/parser/ParserUtil.java
@@ -123,14 +123,6 @@ public class ParserUtil {
         return tagSet;
     }
 
-    /**
-     * Parses a {@code String syllabus} into an {@code Syllabus}.
-     * Leading and trailing whitespaces will be trimmed.
-     * @param syllabus input syllabus to be checked
-     * @return a valid {@code Syllabus}
-     *
-     * @throws ParseException if the given {@code String syllabus) is invalid.
-     */
     public static Syllabus parseSyllabus(String syllabus) throws ParseException {
         requireNonNull(syllabus);
         String trimmedSyllabus = syllabus.trim();
