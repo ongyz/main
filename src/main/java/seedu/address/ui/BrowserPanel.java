0e91d9bb7cbb1584306369526dfd33188bda632d
SusterDrifter <anthonyhalim1997@gmail.com>
2018-10-09 17:56:24 +0800

    Change address/ui/BrowserPanel.java: edit style violation

--- src/main/java/seedu/address/ui/BrowserPanel.java
+++ src/main/java/seedu/address/ui/BrowserPanel.java
@@ -2,12 +2,12 @@ package seedu.address.ui;
 
 import java.net.URL;
 import java.util.logging.Logger;
+
 import javafx.application.Platform;
 import javafx.event.Event;
 import javafx.fxml.FXML;
 import javafx.scene.layout.Region;
 import javafx.scene.web.WebView;
-
 import seedu.address.MainApp;
 import seedu.address.commons.core.LogsCenter;
 import seedu.address.commons.events.ui.PersonPanelSelectionChangedEvent;
