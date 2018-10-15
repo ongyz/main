3bd49c8f73f4e13cf73874d0903d0fa2c0bf78ab
SusterDrifter <anthonyhalim1997@gmail.com>
2018-10-15 12:14:24 +0800

    Rename logic/parser/CopySyllCommandParser.java: to CopySubCommandParser.java

--- src/main/java/seedu/address/logic/parser/CopySubCommandParser.java
+++ src/main/java/seedu/address/logic/parser/CopySubCommandParser.java
@@ -2,36 +2,31 @@ package seedu.address.logic.parser;
 
 import static java.util.Objects.requireNonNull;
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
-import static seedu.address.logic.commands.AppendSyllCommand.AppendSyllFormatChecker.PERSON_INDEX_LOCATION;
-import static seedu.address.logic.commands.AppendSyllCommand.AppendSyllFormatChecker.SUBJECT_INDEX_LOCATION;
-import static seedu.address.logic.commands.AppendSyllCommand.AppendSyllFormatChecker.TODO_NUMBER_OF_ARGS;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_SYLLABUS;
+import static seedu.address.logic.commands.CopySubCommand.CopySubFormatChecker.SUBJECT_INDEX;
+import static seedu.address.logic.commands.CopySubCommand.CopySubFormatChecker.NUMBER_OF_ARGS;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
 
 import java.util.List;
 
 import seedu.address.commons.core.index.Index;
-import seedu.address.logic.commands.AppendSyllCommand;
-import seedu.address.logic.commands.CopySyllCommand;
+import seedu.address.logic.commands.CopySubCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
-import seedu.address.model.subject.Syllabus;
 
 /**
  * Parses input arguments and creates a new Appemark ndSyllCommand object
  */
-public class CopySyllCommandParser implements Parser<CopySyllCommand> {
+public class CopySubCommandParser implements Parser<CopySubCommand> {
 
     /**
      * Parses the given {@code String} of arguments in the context of the AppendSyllCommand
      * and returns an AppendSyllCommand object for execution.
      * @throws ParseException if the user input does not conform the expected format
      */
-    public AppendSyllCommand parse(String args) throws ParseException {
+    public CopySubCommand parse(String args) throws ParseException {
         requireNonNull(args);
-        ArgumentMultimap argMultimap = ArgumentTokenizer.tokenize(args, PREFIX_SYLLABUS);
+        ArgumentMultimap argMultimap = ArgumentTokenizer.tokenize(args, PREFIX_NAME);
 
-        Index personIndex;
         Index subjectIndex;
-        List<Index> indexList;
 
         try {
             indexList = ParserUtil.parseIndexes(argMultimap.getPreamble());
@@ -40,7 +35,7 @@ public class CopySyllCommandParser implements Parser<CopySyllCommand> {
                     String.format(MESSAGE_INVALID_COMMAND_FORMAT, AppendSyllCommand.MESSAGE_USAGE), pe);
         }
 
-        if (indexList.size() != TODO_NUMBER_OF_ARGS) {
+        if (indexList.size() != NUMBER_OF_ARGS) {
             throw new ParseException(
                     String.format(MESSAGE_INVALID_COMMAND_FORMAT, AppendSyllCommand.MESSAGE_USAGE));
         }
@@ -52,16 +47,15 @@ public class CopySyllCommandParser implements Parser<CopySyllCommand> {
             throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, AppendSyllCommand.MESSAGE_USAGE));
         }
 
-        Syllabus syllabus = ParserUtil.parseSyllabus(argMultimap.getValue(PREFIX_SYLLABUS).get());
         return new AppendSyllCommand(personIndex, subjectIndex, syllabus);
     }
 
     private static Index getPersonIndex(List<Index> indexList) {
-        return indexList.get(PERSON_INDEX_LOCATION);
+        return indexList.get(PERSON_INDEX);
     }
 
     private static Index getSubjectIndex(List<Index> indexList) {
-        return indexList.get(SUBJECT_INDEX_LOCATION);
+        return indexList.get(SUBJECT_INDEX);
     }
 
 }
