ed6184f395e9333940dc89fc39baff92ad1f9ba9
ongyz <ongyanzhi96@gmail.com>
2018-10-24 14:48:22 +0800

    Edit Person.java: isSamePerson checks all identity fields

--- src/main/java/seedu/address/model/person/Person.java
+++ src/main/java/seedu/address/model/person/Person.java
@@ -109,8 +109,8 @@ public class Person {
         return otherPerson != null
                 && otherPerson.getName().equals(getName())
                 && (otherPerson.getPhone().equals(getPhone())
-                || otherPerson.getEmail().equals(getEmail())
-                || otherPerson.getAddress().equals(getAddress()));
+                && otherPerson.getEmail().equals(getEmail())
+                && otherPerson.getAddress().equals(getAddress()));
     }
 
     /**
