bcc0fcea9d1349ced7932bd0c33901e5059687da
andlimey <andychan1451@gmail.com>
2018-11-04 13:42:57 +0800

    Change name regex to accept only alphabetical inputs and fix the corresponding test cases

--- src/test/java/seedu/address/model/person/NameContainsKeywordsPredicateTest.java
+++ src/test/java/seedu/address/model/person/NameContainsKeywordsPredicateTest.java
@@ -67,6 +67,7 @@ public class NameContainsKeywordsPredicateTest {
         predicate = new NameContainsKeywordsPredicate(Arrays.asList("Carol"));
         assertFalse(predicate.test(new PersonBuilder().withName("Alice Bob").build()));
 
+
         // Keywords match phone, email and address, but does not match name
         predicate = new NameContainsKeywordsPredicate(Arrays.asList("12345678", "alice@email.com", "Main", "Street"));
         assertFalse(predicate.test(new PersonBuilder().withName("Alice").withPhone("12345678")
