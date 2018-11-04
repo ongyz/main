608ca297351d030885e0742dbd8c6250b5c92438
SusterDrifter <anthonyhalim1997@gmail.com>
2018-11-04 18:54:24 +0800

    Fix to subject parsing to prevent mismatching

--- src/main/java/seedu/address/commons/util/StringUtil.java
+++ src/main/java/seedu/address/commons/util/StringUtil.java
@@ -30,7 +30,8 @@ public class StringUtil {
 
         String preppedWord = word.trim();
         checkArgument(!preppedWord.isEmpty(), "Word parameter cannot be empty");
-        checkArgument(preppedWord.split("\\s+").length == 1, "Word parameter should be a single word");
+        checkArgument(preppedWord.split("\\s+").length == 1,
+                "Word parameter should be a single word");
 
         String preppedSentence = sentence;
         String[] wordsInPreppedSentence = preppedSentence.split("\\s+");
@@ -41,26 +42,37 @@ public class StringUtil {
 
     /**
      * Returns true if the {@code word} contains the substring {@code toSearch}.
+     * Match is only counted from the beginning of {@code word}
+     * {@code toSearch} must be of minimum length three before it is considered to
+     * avoid one letter match.
      *   Ignores case, only a partial word match is required.
      *   <br>examples:<pre>
-     *       containsSubWordIgnoreCase("ABcDef", "cd") == true
-     *       containsSubWordIgnoreCase("ABcdEf", "DEF") == true
-     *       containsSubWordIgnoreCase("Abd", "AB") == true
+     *       containsSubWordIgnoreCase("ABcDef", "cd") == false
+     *       containsSubWordIgnoreCase("ABcdEf", "abc") == true
+     *       containsSubWordIgnoreCase("Abd", "AB") == false // length less than three
      *       </pre>
      * @param word cannot be null
      * @param toSearch cannot be null, cannot be empty, must be a single word
      */
-    public static boolean containsSubstringIgnoreCase(String word, String toSearch) {
+    public static boolean isSubstringMatchFromIndexZero(String word, String toSearch) {
         requireNonNull(word);
         requireNonNull(toSearch);
-
         String preppedToSearch = toSearch.trim();
         checkArgument(!preppedToSearch.isEmpty(), "Word parameter cannot be empty");
-        checkArgument(preppedToSearch.split("\\s+").length == 1, "Word parameter should be a single word");
+        checkArgument(preppedToSearch.split("\\s+").length == 1,
+                "Word parameter should be a single word");
+        // Length of keyword must exceed 2
+        if (preppedToSearch.length() <= 2) {
+            return false;
+        }
+        for (int i = 0; i < preppedToSearch.length(); i++) {
+            if (Character.toLowerCase(word.charAt(i)) != Character.toLowerCase(preppedToSearch.charAt(i))) {
+                return false;
+            }
+        }
 
-        return Pattern.compile(Pattern.quote(toSearch), Pattern.CASE_INSENSITIVE).matcher(word).find();
+        return true;
     }
-
     /**
      * Returns a detailed message of the t, including the stack trace.
      */
