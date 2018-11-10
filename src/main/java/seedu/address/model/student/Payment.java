d4e5718fe83e682e03cfcf05b56b0c0850790419
ooihuiying <huang.ooihuiying@gmail.com>
2018-11-10 18:12:29 +0800

    Minor changes to payment message

--- src/main/java/seedu/address/model/student/Payment.java
+++ src/main/java/seedu/address/model/student/Payment.java
@@ -16,9 +16,9 @@ public class Payment {
     public static final String MESSAGE_PAYMENT_AMOUNT_CONSTRAINTS =
             "Amount should only contain positive integers between 0 to 10 000.";
     public static final String MESSAGE_PAYMENT_MONTH_CONSTRAINTS =
-            "Month should only contain numbers between 1 to 12, inclusive.";
+            "Month should only contain integers between 1 to 12, inclusive.";
     public static final String MESSAGE_PAYMENT_YEAR_CONSTRAINTS =
-            "Year should only contain 4 digits numbers.";
+            "Year should only contain 4 digits integers.";
 
     public static final String TAG_VALIDATION_REGEX = "(.)*(\\d)(.)*";
     private static final int MAXPAYMENTAMOUNT = 10000;
