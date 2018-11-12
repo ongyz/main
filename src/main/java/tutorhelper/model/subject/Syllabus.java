3c4e6a41395dca6c43c71a5062c16b2c3d5ad2cc
ooihuiying <huang.ooihuiying@gmail.com>
2018-11-12 10:24:23 +0800

    Resolve issue for syllabus to check for special character instead of len

--- src/main/java/tutorhelper/model/subject/Syllabus.java
+++ src/main/java/tutorhelper/model/subject/Syllabus.java
@@ -9,8 +9,8 @@ import static tutorhelper.commons.util.AppUtil.checkArgument;
  */
 public class Syllabus {
 
-    public static final String MESSAGE_SYLLABUS_CONSTRAINTS = "Syllabus can take any values, and it should not be "
-            + "blank or preceded by white space. Syllabus should not be longer than 30 characters.";
+    public static final String MESSAGE_SYLLABUS_CONSTRAINTS = "Syllabus can take any values except for the special character '/', and it should not be "
+            + "blank or preceded by white space.";
 
     /*
      * The first character of the syllabus must not be a whitespace, otherwise " " (a blank string)
@@ -50,9 +50,11 @@ public class Syllabus {
      * Returns true if a given string is a valid syllabus.
      */
     public static boolean isValidSyllabus(String test) {
-        if (test.length() > SYLLABUS_VALIDATION_MAX_LENGTH) {
-            return false;
-        } else {
+
+        if (test.contains("/")) {
+           return false;
+        }
+        else {
             return test.matches(SYLLABUS_VALIDATION_REGEX);
         }
     }
