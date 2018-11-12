487019e6de04c86b0d74a8a2f41272ec2998d3f2
ooihuiying <huang.ooihuiying@gmail.com>
2018-11-12 20:29:16 +0800

    improve code style for payCommandParser

--- src/main/java/tutorhelper/logic/parser/PayCommandParser.java
+++ src/main/java/tutorhelper/logic/parser/PayCommandParser.java
@@ -35,32 +35,27 @@ public class PayCommandParser implements Parser<PayCommand> {
             throw new ParseException(
                     String.format(MESSAGE_INVALID_COMMAND_FORMAT, PayCommand.MESSAGE_USAGE));
         }
-        String inputStudentIndex = separatedPayment[0];
-        String inputAmount = separatedPayment[1];
-        String inputMonth = separatedPayment[2];
-        String inputYear = separatedPayment[3];
-
         //Put the arguments into ParserUtil to check for validity
         try {
-            this.studentIndex = ParserUtil.parseIndex(inputStudentIndex);
+            this.studentIndex = ParserUtil.parseIndex(separatedPayment[0]);
         } catch (ParseException e) {
             throw new ParseException(
                     String.format(MESSAGE_INVALID_INDEX, PayCommand.MESSAGE_USAGE), e);
         }
         try {
-            this.amount = ParserUtil.parseAmount(inputAmount);
+            this.amount = ParserUtil.parseAmount(separatedPayment[1]);
         } catch (ParseException e) {
             throw new ParseException(
                     String.format(Payment.MESSAGE_PAYMENT_AMOUNT_CONSTRAINTS, PayCommand.MESSAGE_USAGE), e);
         }
         try {
-            this.month = ParserUtil.parseMonth(inputMonth);
+            this.month = ParserUtil.parseMonth(separatedPayment[2]);
         } catch (ParseException e) {
             throw new ParseException(
                     String.format(Payment.MESSAGE_PAYMENT_MONTH_CONSTRAINTS, PayCommand.MESSAGE_USAGE), e);
         }
         try {
-            this.year = ParserUtil.parseYear(inputYear);
+            this.year = ParserUtil.parseYear(separatedPayment[3]);
         } catch (ParseException e) {
             throw new ParseException(
                     String.format(Payment.MESSAGE_PAYMENT_YEAR_CONSTRAINTS, PayCommand.MESSAGE_USAGE), e);
